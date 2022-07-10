4544d87e93ed000ca8c4d8d42c8375d9f4a239be
AkasakaID <fawwazthoerif@gmail.com>
2022-07-10 20:33:52 +0700

    :update ke-*ngak tau ke berapa*

--- perintah
+++
@@ -1,1 +0,0 @@
-npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['./index.html'],
   theme: {
-    container:{
-      center: true,
-      padding: '16px',
-    },
-    extend: {
-      screens: {
-        '2xl': '1280px',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
