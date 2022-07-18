4eacce5efdb7eaf371c2466b986b0088eefc351e
AkasakaID <fawwazthoerif@gmail.com>
2022-07-18 09:50:12 +0700

    :update index.html & add somefile

---
+++ id/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    fontFamily: {
+      comfortaa: 'Comfortaa',
+      arial: 'Arial'
+    },
+    extend: {
+      width:{
+        '300':'300px',
+        '320':'320px',
+        '600':'600px',
+        '650':'650px',
+      }
+    },
+  },
+  plugins: [],
+}
